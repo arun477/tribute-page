7bf5ee2a945e2604f29e327958f04c25a449d7b3
Arun <coolarun477@gmail.com>
2017-04-09 01:04:21 -0700

    Add files via upload

---
+++ My Kalam.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+  $(".biopara").fadeOut();
+
+  $("#biohead").click(function(){
+    $(".biopara").toggle();
+
+  });
+
+  $(".awarda").fadeOut();
+    $("#awards").click(function(){
+    $(".awarda").toggle();});
+
+  $(".booksa").fadeOut();
+    $("#books").click(function(){
+    $(".booksa").toggle();});
+
+
+  $(".contrib").fadeOut();
+    $("#contributions").click(function(){
+    $(".contrib").toggle();});
+
+
+
+
+
+});
